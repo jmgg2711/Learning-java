97d22236180cbfb3662d64b0a8437157dfb76e59
Juan Gutierrez <juan.m.gutierrez.g@gmail.com>
2023-10-29 14:22:28 -0700

    .

--- ClasesObjetos/TestFecha2.java
+++ ClasesObjetos/TestFecha2.java
@@ -2,7 +2,7 @@ package ClasesObjetos;
 
 public class TestFecha2 {
     public static void main(String[]args){
-        Fecha f;
+        Fecha f = new Fecha();
         f.setDia(27);
         f.setMes(11);
         f.setAnio(1976);
