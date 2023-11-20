1a99770f77bb282910232e94ddcb5248255a3372
Juan Gutierrez <juan.m.gutierrez.g@gmail.com>
2023-11-19 23:11:05 -0800

    se agregan comentarios en la creación de objetos

--- Polimorfismo/TestPolimorfismo.java
+++ Polimorfismo/TestPolimorfismo.java
@@ -1,12 +1,14 @@
 package Polimorfismo;
 
-import ClasesObjetos.*;
-import Herencia.*;
+import ClasesObjetos.Fecha;
+import Herencia.FechaDetallada;
 
 public class TestPolimorfismo {
     public static void main(String[] args){
+        // a fec (de tipo Fecha) le asigno un objeto FechaDetallada
         Fecha fec = new FechaDetallada("27/11/1976");
 
+        // a obj (de tipo objeto) le asigno un objeto de FechaDetallada
         Object obj = new FechaDetallada("23/12/1977");
 
         System.out.println("fec: " + fec); //invoca a toString de fec
